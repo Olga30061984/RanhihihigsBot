dc09687f4dba98bc0b7006ba830587e9ef0aaddf
Nikita Pyshkov <85805562+pyshkovni@users.noreply.github.com>
2024-09-12 18:41:42 +0000

    Запустили проект бота с командой /start

--- main.py
+++ main.py
@@ -1,10 +1,38 @@
 ## Этап 1. Начало работы с aiogram3
 
-# Установить aiogram3
-# pip install aiogram3
+# Дополнительный гайд по aiogram3 https://mastergroosha.github.io/aiogram-3-guide/
+# Документация по aiogram3 https://docs.aiogram.dev/en/latest/
+# Ссылка на форум aiogram в тг: https://t.me/aiogram
+
+# 0. Установить aiogram3
+# pip install aiogram
+
+# 1. Импорт
+import asyncio  # асинхронный ввод-вывод
+from aiogram import Bot, Dispatcher, types, filters  # класс бота и диспетчера
+from config import TOKEN
+
+# 2. Экзампляры бота и диспетчера
+bot = Bot(TOKEN)
+dp = Dispatcher()
+
+# Все обработчики должны быть подключены к маршрутизатору (или диспетчеру)
+# Обработчики (handlers) — обработчик сообщений, который будет возвращать другое сообщение, указанное в функции
 
 
+# 3. Создание обработчиков
+# -- Обработчик (handler) --
+@dp.message(filters.CommandStart())  # декоратора диспетчера
+async def command_start_handler(message: types.Message) -> None:
+    # await message.answer("Hello!")
+    await message.answer(f"Hello, {message.from_user.username}!")
 
-# TODO
-# - aiogram3
 
+# -- Вспомогательная функция (utils) --
+async def main() -> None:
+    await dp.start_polling(bot)
+
+
+# 4. Запуск
+if __name__ == "__main__":
+    asyncio.run(main())
