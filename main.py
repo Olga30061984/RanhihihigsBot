97a1e777f75c0c7ed571e18c26425a59d1c14599
pyshkovni <guess.the.tutor@gmail.com>
2024-09-23 20:51:39 +0300

    Описали логирование для основных функций

--- main.py
+++ main.py
@@ -14,11 +14,18 @@
 # TODO - сделать маршрутизацию вместо декораторов
 
 # 1. Импорт
+import logging  # чтобы отследить состояние бота, используем логи
 import asyncio  # асинхронный ввод-вывод
 from aiogram import Bot, Dispatcher, types, filters  # класс бота и диспетчера
 from config import TOKEN
 
-# 2. Экзампляры бота и диспетчера
+
+# 3. Установить общий уровень логирования и создали экземпляр лога
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
+
+# 3. Экзампляры бота и диспетчера
 bot = Bot(TOKEN)
 dp = Dispatcher()
 
@@ -26,27 +33,31 @@ dp = Dispatcher()
 # Обработчики (handlers) — обработчик сообщений, который будет возвращать другое сообщение, указанное в функции
 
 
-# 3. Создание обработчиков
+# 4. Создание обработчиков
 # -- Обработчики (handler) --
 @dp.message(filters.CommandStart())  # декоратора диспетчера
 async def command_start_handler(message: types.Message) -> None:
     """команда /start"""
     # await message.answer("Hello!")
     await message.answer(f"Hello, {message.from_user.username}!")
+    logger.info(f"user {message.from_user.id} starts bot!")
 
 
 @dp.message()
 async def echo_handler(message: types.Message) -> None:
     """эхо-ответ"""
     await message.send_copy(chat_id=message.chat.id)
+    logger.info(f"user {message.from_user.id} send text and get echo!")
 
 
 # -- Вспомогательная функция (utils) --
 async def main() -> None:
     """polling-запуск проекта"""
+
+    # polling-запуск
     await dp.start_polling(bot)
 
 
-# 4. Запуск
+# 5. Запуск
 if __name__ == "__main__":
     asyncio.run(main())
